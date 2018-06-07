4ad58a51d240ef12ec2ede2fab21c218e3d4b754
zepedrorafael <36938779+zepedrorafael@users.noreply.github.com>
2018-06-07 16:00:02 +0100

    update models

--- Application/Model/Departamentos.php
+++ Application/Model/Departamentos.php
@@ -4,46 +4,46 @@ use Config as Conf;
 
 class Departamento {
     
-    private $Id;
+    private $id;
     private $nome;
     private $observacoes;
     
-    public function getId(){
-        return $this->Id;
+    public function getid(){
+        return $this->id;
     }
     
-    public function getNome(){
+    public function getnome(){
         return $this->nome;
     }    
     
-    public function getObservacoes(){
+    public function getobservacoes(){
         return $this->observacoes;
     }    
     
-    public function setId($value){
-        $this->Id = $value;        
+    public function setid($value){
+        $this->id = $value;        
     }
     
-    public function setNome($value){     
+    public function setnome($value){     
         $this->nome = $value;
     }
     
-    public function setObservacoes($value){      
+    public function setobservacoes($value){      
         $this->observacoes = $value;
     }    
     
 
     
     public function convertObjectToArray(){
-        $data = array(  'Id' => $this->getId(), 
-                        'nome' => $this->getNome(),
-                        'observacoes' => $this->getObservacoes());        
+        $data = array(  'id' => $this->getid(), 
+                        'nome' => $this->getnome(),
+                        'observacoes' => $this->getobservacoes());        
         
         return $data;
     }
     
     public static function convertArrayToObject(Array &$data){
-        return self::createObject($data['Id'], $data['nome'], $data['observacoes']);
+        return self::createObject($data['id'], $data['nome'], $data['observacoes']);
     }    
     
     public static function createObject($id, $nome, $observacoes){
