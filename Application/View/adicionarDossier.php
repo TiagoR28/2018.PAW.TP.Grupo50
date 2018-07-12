2107bd17ad508b97f9af853d2f89147514180566
Hugo Napoleão <hugo_1992napoleao@hotmail.com>
2018-07-12 18:35:41 +0100

    Adiocionar dossier ja esta ligado ao index

--- Application/View/adicionarDossier.php
+++ Application/View/adicionarDossier.php
@@ -28,7 +28,7 @@ session_start();
                 <nav class="navbar navbar-tranparent">
                     <div class="container-fluid">
                         <div class="navbar-header">
-                            <img class="navbar-brand" src="<?= Conf::getApplicationImagePath() . 'mRFd2kaT_400x400.png' ?>" alt="LOGO">
+                            <a href="<?= Conf::getApplicationViewPath() . 'perfilUser.php' ?>"><img class="navbar-brand" src="<?= Conf::getApplicationImagePath() . 'mRFd2kaT_400x400.png' ?>" alt="LOGO"></a>
                         </div>
                         <button onclick="location.href = '<?= Conf::getApplicationViewPath() . 'index.php' ?>';" class="btn btn-danger navbar-btn">LogOut</button>
                     </div>
